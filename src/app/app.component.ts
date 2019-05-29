043f8e5f7a4b0c06bc8e92ce23e9ed4d218f7f4f
sruiz <samuel.ruiz@atalantago.com>
2019-05-29 09:46:11 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eba-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'easy-bulma-angular';
+}
